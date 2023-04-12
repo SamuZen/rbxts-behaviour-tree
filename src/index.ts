a93e8fd82ae6aa52132e750fe51bf08d42333a41
SamuZen <Pedroqueiroz.zen@gmail.com>
2023-04-12 09:30:23 -0300

    Add selector node

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,4 @@ export { NodeStatus } from "./NodeStatus";
 export { Action } from "./Nodes/Action";
 export { Condition } from "./Nodes/Condition";
 export { Sequence } from "./Nodes/Sequence";
+export { Selector } from "./Nodes/Selector";
