c848dd3a17872def910d9a7e055695bc69f8cdc9
SamuZen <Pedroqueiroz.zen@gmail.com>
2023-04-27 20:47:47 -0300

    Add Possibility for Actions return NodeState

--- src/Nodes/Action.ts
+++ src/Nodes/Action.ts
@@ -3,15 +3,19 @@ import { Blackboard } from "../Blackboard";
 import { NodeStatus } from "../NodeStatus";
 
 export class Action extends Node {
-	action: (blackboard: Blackboard) => void;
+	action: (blackboard: Blackboard) => NodeStatus | void;
 
-	constructor(action: (blackboard: Blackboard) => void) {
+	constructor(action: (blackboard: Blackboard) => NodeStatus | void) {
 		super();
 		this.action = action;
 	}
 
 	tick(blackboard: Blackboard) {
-		this.action(blackboard);
-		this.status = NodeStatus.SUCCESS;
+		const actionResult = this.action(blackboard);
+		if (actionResult === undefined) {
+			this.status = NodeStatus.SUCCESS;
+		} else {
+			this.status = actionResult;
+		}
 	}
 }
